097663de00e3ef986f12ed151956a1e97008eeaa
Sarah Dowling <72190858+sarahloui-se@users.noreply.github.com>
2022-10-05 21:59:30 +1100

    Initial commit of existing progress

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+const toggle = document.querySelector(".toggle"); // Define CSS class .toggle as a constant named toggle
+const menu = document.querySelector("#menu");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<span class='fas fa-bars'></span>";
+    } else {
+        menu.classList.add("active");
+
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<span class='fas fa-times'></span>";
+    }
+}
+
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
